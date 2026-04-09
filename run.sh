16194d7a632af64eb6e5e4689e4862be73b90bdb
Jens Friberg <association@live.se>
2026-04-09 17:53:53 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
+
 curl -O https://raw.githubusercontent.com/Association32/VT_26_ML_part3/main/embeddings.py
 curl -O https://raw.githubusercontent.com/Association32/VT_26_ML_part3/main/evaluation.py
-curl -O https://raw.githubusercontent.com/Association32/VT_26_ML_part3/labels.txt
-curl -O https://raw.githubusercontent.com/Association32/VT_26_ML_part3/neural.py
-curl -O https://raw.githubusercontent.com/Association32/VT_26_ML_part3/test.tsv
-curl -O https://raw.githubusercontent.com/Association32/VT_26_ML_part3/train.tsv
