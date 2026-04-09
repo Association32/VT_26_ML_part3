d220e41e0dbf10a2c8bdbadb836e946fcf1a2f07
Jens Friberg <association@live.se>
2026-04-09 18:28:06 +0200

    Create FNN.py

---
+++ FNN.py
@@ -0,0 +1,12 @@
+class FFN(nn.Module):
+    def __init__(self, input_dim, num_classes):
+        super().__init__()
+        self.net = nn.Sequential(
+            ## So here is the pattern of layers
+            nn.Linear(input_dim, 128),
+            nn.ReLU(),
+            nn.Linear(128, num_classes)
+        )
+
+    def forward(self, x):
+        return self.net(x)
