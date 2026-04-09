8bcf2ce8cce8e9b020fde81652fa560e6433fece
Jens Friberg <association@live.se>
2026-04-09 20:42:42 +0200

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
 curl -o embeddings.py https://raw.githubusercontent.com/Association32/VT_26_ML_part3/main/embeddings.py
 curl -o evaluation.py https://raw.githubusercontent.com/Association32/VT_26_ML_part3/main/evaluation.py
 curl -o neural.py https://raw.githubusercontent.com/Association32/VT_26_ML_part3/main/neural.py
 curl -o train.tsv https://raw.githubusercontent.com/Association32/VT_26_ML_part3/main/train.tsv
 curl -o test.tsv https://raw.githubusercontent.com/Association32/VT_26_ML_part3/main/test.tsv
 curl -o labels.txt https://raw.githubusercontent.com/Association32/VT_26_ML_part3/main/labels.txt
+curl -o utils.py https://raw.githubusercontent.com/Association32/VT_26_ML_part3/main/utils.py
